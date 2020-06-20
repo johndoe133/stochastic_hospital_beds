4fda00905cf478f7940c50c179739420b3f156b8
DeterjoSimon <43176887+DeterjoSimon@users.noreply.github.com>
2020-06-20 19:27:25 +0200

    Cleared out the types and documentation for each function.

--- main.py
+++ main.py
@@ -1,44 +1,66 @@
 import numpy as np
 
-class simulation():
+
+class Simulation:
     def __init__(self, beds):
+        """
+        :param beds: Max number of beds allocated for this simulation
+        """
         self.beds = beds
         self.current_time = 0
-        self.los = [[],[],[]] # List of list of length of stays los[0] is the list of when patients are staying until for ward a
+        self.los = {k: [] for k in ["A", "B", "C"]}  # Dictionary of length of stays los[0] is the list of when 
+        # patients are staying until for ward a 
         self.next_arrivals = []
-        self.blocked = [0,0,0]
-        self.total = [0,0,0]
+        self.blocked = [0, 0, 0]
+        self.total = [0, 0, 0]
 
     def next_arrival(self, ward):
-        # Return current_time + itnerarrival time for a specific ward
-        # ward = 0, 1, 2 for a, b, c
+        """
+        :type ward: str
+        :param ward: choose ward between a, b or c.
+        :return: the current time t AND the next arrival for this specific ward.
+        """
         return 0
 
     def next_los(self, ward):
-        # Return current_time + los time for a specific ward
-        # ward = 0, 1, 2 for a, b, c
+        """
+        :type ward: str
+        :param ward: choose ward between a, b or c.
+        :return: the current time t AND the length of stay (los) in this ward.
+        """
         return 0
 
     def get_next_event(self):
-        # Return the time of the next event to occur
-        # This looks through next_arrivals and los for smallest value
+        """
+        This looks through next_arrivals and los for smallest value.
+        :return: Time when the next event occurs.
+        """
         return 0
 
     def increment_time(self):
-        # Change the time to the next event
+        """
+        :return: Change the time t.
+        """
         self.current_time = self.get_next_event()
         return 0
-    
+
     def check_in(self):
-        # Check in a patient to a ward
+        """
+        :return: Check in a patient to a ward
+        """
         return 0
 
     def check_out(self):
-        # Check a patient out of a ward
+        """
+        :return: Check a patient out of a ward
+        """
         return 0
 
     def do_current_event(self):
-        # Do the event occurring now depending on the time
+        """
+        Activate the event occurring at time t.
+        :return:
+        """
 
         if self.current_time in self.next_arrivals:
             # Check which ward they are joining and add them
@@ -50,6 +72,10 @@ class simulation():
             return 0
 
     def run_simulation(self):
+        """
+        Runs the simulation.
+        :return:
+        """
         # reset current time
         self.current_time = 0
 
@@ -57,11 +83,9 @@ class simulation():
         self.next_arrivals += [self.next_arrival(i) for i in range(3)]
         while self.current_time < 365:
             # Increment time to next event
-            self.increment_time() 
+            self.increment_time()
 
             # Do the event at the current time
             self.do_current_event()
             # This function should change the los or next_arrivals variable
             # It should also adjust self.blocked and self.total (number of patients)
-            
-       
